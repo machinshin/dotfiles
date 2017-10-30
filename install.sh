4bbdcb24ef80ac2d2539012a987cacb7f323b3e4
Black Wind <machinshin@gmail.com>
2017-10-29 23:44:04 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-git clone --bare https://github.com/machinshin/dotfiles.git $HOME/.dotfgit
+git clone --bare https://github.com/machinshin/dotfiles.git $HOME/.dotfiles.git
 function config {
-   /usr/local/bin/git --git-dir=$HOME/.dotfgit/ --work-tree=$HOME $@
+   /usr/local/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@
 }
-mkdir -p .dotfgit-backup
+mkdir -p .dotfiles.git-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfgit-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.git-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
